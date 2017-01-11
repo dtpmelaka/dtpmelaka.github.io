c81602072db3d1e68b8f080239e12111b615e8eb
Ryan Tan <ryan_zhiyou@hotmail.com>
2017-01-11 09:11:16 +0800

    [update] pushed file from computer

--- dragndrop/JavaScript.js
+++ dragndrop/JavaScript.js
@@ -1,69 +1,31 @@
-﻿(function (exports) {
-    function urlsToAbsolute(nodeList) {
-        if (!nodeList.length) {
-            return [];
-        }
-        var attrName = 'href';
-        if (nodeList[0].__proto__ === HTMLImageElement.prototype
-        || nodeList[0].__proto__ === HTMLScriptElement.prototype) {
-            attrName = 'src';
-        }
-        nodeList = [].map.call(nodeList, function (el, i) {
-            var attr = el.getAttribute(attrName);
-            if (!attr) {
-                return;
-            }
-            var absURL = /^(https?|data):/i.test(attr);
-            if (absURL) {
-                return el;
-            } else {
-                return el;
-            }
-        });
-        return nodeList;
-    }
+﻿//(function (exports) {
+function capture() {
+    alert("click");
+    toDataUrl('https://www.google.com/search?q=google+image&espv=2&biw=1366&bih=638&tbm=isch&imgil=o_gRCLMCFpYWZM%253A%253BnUneulojzU3VnM%253Bhttps%25253A%25252F%25252Fwww.youtube.com%25252Fuser%25252FGoogle&source=iu&pf=m&fir=o_gRCLMCFpYWZM%253A%252CnUneulojzU3VnM%252C_&usg=__oUMpVo97oXsT-PJ5ZbhikTtTnEA%3D', function (base64Img) {
+        console.log(base64Img);
+    }, );
+}
 
-    function screenshotPage() {
-        urlsToAbsolute(document.images);
-        urlsToAbsolute(document.querySelectorAll("link[rel='stylesheet']"));
-        var screenshot = document.documentElement.cloneNode(true);
-        var b = document.createElement('base');
-        b.href = document.location.protocol + '//' + location.host;
-        var head = screenshot.querySelector('head');
-        head.insertBefore(b, head.firstChild);
-        screenshot.style.pointerEvents = 'none';
-        screenshot.style.overflow = 'hidden';
-        screenshot.style.webkitUserSelect = 'none';
-        screenshot.style.mozUserSelect = 'none';
-        screenshot.style.msUserSelect = 'none';
-        screenshot.style.oUserSelect = 'none';
-        screenshot.style.userSelect = 'none';
-        screenshot.dataset.scrollX = window.scrollX;
-        screenshot.dataset.scrollY = window.scrollY;
-        var script = document.createElement('script');
-        script.textContent = '(' + addOnPageLoad_.toString() + ')();';
-        screenshot.querySelector('body').appendChild(script);
-        var blob = new Blob([screenshot.outerHTML], {
-            type: 'text/html'
-        });
-        return blob;
+function toDataUrl(src, callback, outputFormat) {
+    var img = new Image();
+    img.crossOrigin = 'Anonymous';
+    img.onload = function () {
+        var canvas = document.createElement('CANVAS');
+        var ctx = canvas.getContext('2d');
+        var dataURL;
+        canvas.height = this.height;
+        canvas.width = this.width;
+        ctx.drawImage(this, 0, 0);
+        dataURL = canvas.toDataURL(outputFormat);
+        callback(dataURL);
+    };
+    img.src = src;
+    if (img.complete || img.complete === undefined) {
+        img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+        img.src = src;
     }
-
-    function addOnPageLoad_() {
-        window.addEventListener('DOMContentLoaded', function (e) {
-            var scrollX = document.documentElement.dataset.scrollX || 0;
-            var scrollY = document.documentElement.dataset.scrollY || 0;
-            window.scrollTo(scrollX, scrollY);
-        });
-    }
-
-    function generate() {
-        window.URL = window.URL || window.webkitURL;
-        window.open(window.URL.createObjectURL(screenshotPage()));
-    }
-    exports.screenshotPage = screenshotPage;
-    exports.generate = generate;
-})(window);
+}
+//})(window);
 
 var full1, full2, full2, full4, full5, full6, full7 = 0;
 var full8, full9, full10, full11, full12, full13, full14 = 0;
