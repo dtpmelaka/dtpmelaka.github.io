91b4a3e8dda4aa7a78bb47eb2e4af5299e56fb73
Ryan Tan <ryan_zhiyou@hotmail.com>
2017-01-13 17:30:45 +0800

    [update] jQuery much better now

--- dragndrop/JavaScript.js
+++ dragndrop/JavaScript.js
@@ -1,19 +1,42 @@
 $(document).ready(function() {
-    $('#drag1, #drag2').draggable({
+    $('#drag_s, #drag_b').draggable({
         create: function(){$(this).data('position',$(this).position())},
         cursor:'move',
+        helper: 'clone',
         start:function(){$(this).stop(true,true)}
     });
 
     $('.dropable').find('td').droppable({
+        accept: '#drag_s, #drag_b',
         drop:function(event, ui){
-            snapToMiddle(ui.draggable,$(this));
+            var weight = $(this).children('#clone_s').length * 4 + $(this).children('#clone_b').length * 6;
+            if (ui.draggable.attr("id") == "drag_s" && weight < 9) {
+                $(this).append(ui.draggable.clone().prop('id', 'clone_s'));
+            }
+            else if (ui.draggable.attr("id") == "drag_b" && weight < 7) {
+                $(this).append(ui.draggable.clone().prop('id', 'clone_b'));
+            }
         }
     });
+
+    $('#drop_zone').droppable({
+        accept: '#drag_s, #drag_b',
+        drop:function(event, ui){
+            if (ui.draggable.attr("id") == "drag_s") {
+                $(this).append(ui.draggable.clone().prop('id', 'clone_s'));
+            }
+            else if (ui.draggable.attr("id") == "drag_b") {
+                $(this).append(ui.draggable.clone().prop('id', 'clone_b'));
+            }
+        }
+    });
+
+    $("#container").resizable();
 });
 
-function snapToMiddle(dragger, target){
-   var topMove = target.position().top - dragger.data('position').top + (target.outerHeight(true) - dragger.outerHeight(true)) / 2;
-   var leftMove= target.position().left - dragger.data('position').left + 10;
-   dragger.animate({top:topMove,left:leftMove},{duration:600,easing:'easeOutBack'});
+function deleteImg(e) {
+    var getID = $(e).attr("id");
+    if (getID == 'clone_s' || getID == 'clone_b') {
+        e.parentNode.removeChild(e);
+    }
 }
